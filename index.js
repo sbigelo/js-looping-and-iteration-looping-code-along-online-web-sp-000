9a4ed6c6687d4f77e0e43961475b076d8bb777a1
sbigelo <sean.bigelow1@gmail.com>
2021-03-31 12:21:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(arr, event) {
+    let new_arr = [];
+    for (let i = 0; i < arr.length; i++) {
+        new_arr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return new_arr;
+}
+
+
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i -= 1
+    }
+
+}
